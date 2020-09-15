cfd05ac9d8a2f4a58ec3c5a5787618cd0d8b5edd
aditiafa <aditiafa112@gmail.com>
2020-09-15 21:46:46 +0700

    init project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use(() => {
+  console.log("hello server");
+  console.log("hello server ....");
+  console.log("hello Aditia");
+});
+
+app.listen(4000);
