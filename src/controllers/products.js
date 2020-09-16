76a491dcb69e8c2474715db5579c4c91d9b7ac74
aditiafa <aditiafa112@gmail.com>
2020-09-16 19:45:30 +0700

    add body-parser

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,6 +1,20 @@
 exports.createProduct = (req, res, next) => {
+  const name = req.body.name;
+  const price = req.body.price;
   res.json({
     message: "Create Product Succes !!!",
+    data: {
+      id: 1,
+      nama: name,
+      price: price,
+    },
+  });
+  next();
+};
+
+exports.getAllProducts = (req, res, next) => {
+  res.json({
+    message: "Get All Products Succes",
     data: {
       id: 1,
       nama: "Sari Roti",
@@ -9,15 +23,3 @@ exports.createProduct = (req, res, next) => {
   });
   next();
 };
-
-exports.getAllProducts = (req, res, next) => {
-    res.json({
-      message: "Get All Products Succes",
-      data: {
-        id: 1,
-        nama: "Sari Roti",
-        price: 8000,
-      },
-    });
-    next();
-  };
